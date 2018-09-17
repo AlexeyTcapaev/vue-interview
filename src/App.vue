f24a8deebb47a60e2ed475f6537217732be2068c
AlexeyTcapaev <lexx1995@mail.ru>
2018-09-17 17:20:57 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-slide-y-transition mode="out-in">
+      </v-slide-y-transition>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Slider from './components/Slider'
+
+export default {
+  name: 'App',
+  components: {
+    Slider
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
